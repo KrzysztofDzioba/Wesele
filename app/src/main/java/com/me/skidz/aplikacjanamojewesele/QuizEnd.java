d35a14bbb06229349320a85c2cf73b3a2a790b4d
KrzysztofDzioba <krzysztof.dzioba.93@gmail.com>
2018-06-09 02:15:06 +0200

    Create onClicks() in q1

---
+++ app/src/main/java/com/me/skidz/aplikacjanamojewesele/QuizEnd.java
@@ -0,0 +1,13 @@
+package com.me.skidz.aplikacjanamojewesele;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuizEnd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_end);
+    }
+}
--- app/src/main/java/com/me/skidz/aplikacjanamojewesele/QuizQuestion1.java
+++ app/src/main/java/com/me/skidz/aplikacjanamojewesele/QuizQuestion1.java
@@ -1,13 +1,72 @@
 package com.me.skidz.aplikacjanamojewesele;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class QuizQuestion1 extends AppCompatActivity {
+
+    private Button q1a1;
+    private Button q1a2;
+    private Button q1a3;
+    private Button q1a4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_question1);
+
+        setButtons();
+        setOnClicks();
+
     }
+
+    private void setButtons() {
+        q1a1 = findViewById(R.id.q1a1);
+        q1a2 = findViewById(R.id.q1a2);
+        q1a3 = findViewById(R.id.q1a3);
+        q1a4 = findViewById(R.id.q1a4);
+    }
+
+    private void setOnClicks() {
+        q1a1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startNextActivity();
+            }
+        });
+
+        q1a2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startNextActivity();
+            }
+        });
+
+        q1a3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startNextActivity();
+            }
+        });
+
+        q1a4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startNextActivity();
+            }
+        });
+    }
+
+    private void startNextActivity() {
+        Class nextActivityClass = QuizEnd.class;
+        Intent nextActivity = new Intent(this, nextActivityClass);
+        startActivity(nextActivity);
+    }
+
+
+
+
 }
